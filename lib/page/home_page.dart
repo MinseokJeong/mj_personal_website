59f601db2145e9fbbf36edfad02dfbf277bf69b1
MJ <minseok.jeong92@gmail.com>
2023-05-07 00:43:42 +0900

    Add arrow icon that point out current mouse pointer so this widget is interactive.

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,55 +1,91 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 import '../widget/hanger_widget.dart';
 import '../widget/horizontal_moving_text_widget.dart';
 import '../app_design_resources.dart' as ar;
+import '../widget/interact_with_mouse_pointer_icon_widget.dart';
 import '../widget/top_header_widget.dart';
 
-class HomePage extends StatelessWidget {
+import 'dart:math' as math;
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _mouseHoverStreamController = StreamController<Offset>();
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     final windowSize = MediaQuery.of(context).size;
-    return Scaffold(
-      backgroundColor: ar.topSectionBackgroundColor,
-      body: Stack(
-        children: [
-          Align(
-            alignment: Alignment.center,
-            child: UnconstrainedBox(
-              clipBehavior: Clip.hardEdge,
-              child: Image.asset(
-                'assets/dennissnellenberg_image.jpeg',
-                height: windowSize.height * 1.2,
-                fit: BoxFit.fitHeight,
+    return MouseRegion(
+      onHover: (event) {
+        _mouseHoverStreamController.add(event.position);
+      },
+      child: Scaffold(
+        backgroundColor: ar.topSectionBackgroundColor,
+        body: Stack(
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: UnconstrainedBox(
+                clipBehavior: Clip.hardEdge,
+                child: Image.asset(
+                  'assets/dennissnellenberg_image.jpeg',
+                  height: windowSize.height * 1.2,
+                  fit: BoxFit.fitHeight,
+                ),
               ),
             ),
-          ),
-          Align(alignment: Alignment.topCenter, child: TopHeaderWidget()),
-          Align(
-              alignment: Alignment(-1.0, 0.0),
-              child: HangerWidget(windowSize: windowSize)),
-          Align(
-            child: Text(
-              'Software\nEngineer & Developer',
-              style: ar.textStyleTopSectionLarge,
-            ),
-            alignment: Alignment(0.9, 0.0),
-          ),
-          Align(
-            alignment: Alignment(-1.0, 0.9),
-            child: HorizontalMovingTextWidget(
-              text: "Minseok Jeong ",
-              textStyle: TextStyle(
-                color: Colors.white,
-                fontSize: 150.0,
-                fontFamily: ar.FontName.NotoSansKorean.name,
+            Align(alignment: Alignment.topCenter, child: TopHeaderWidget()),
+            Align(
+                alignment: Alignment(-1.0, 0.0),
+                child: HangerWidget(windowSize: windowSize)),
+            Align(
+              child: Text(
+                'Software\nEngineer & Developer',
+                style: ar.textStyleTopSectionLarge,
               ),
-              speed: 22000,
-              direction: MovingTextDirection.moving_left,
+              alignment: Alignment(0.9, 0.0),
             ),
-          ),
-        ],
+            Align(
+              alignment: Alignment(-1.0, 0.9),
+              child: HorizontalMovingTextWidget(
+                text: "Minseok Jeong ",
+                textStyle: TextStyle(
+                  color: Colors.white,
+                  fontSize: 150.0,
+                  fontFamily: ar.FontName.NotoSansKorean.name,
+                ),
+                speed: 22000,
+                direction: MovingTextDirection.moving_left,
+              ),
+            ),
+            Align(
+              alignment: Alignment(0.6, -0.3),
+              child: StreamBuilder<Offset>(
+                stream: _mouseHoverStreamController.stream,
+                builder: (context, snapshot) {
+                  return InteractWithMousePointerIconWidget(
+                    mousePointerOffsetGlobal:
+                        (snapshot.hasData) ? snapshot.data! : null,
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widget/interact_with_mouse_pointer_icon_widget.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class InteractWithMousePointerIconWidget extends StatelessWidget {
+  const InteractWithMousePointerIconWidget(
+      {this.mousePointerOffsetGlobal, Key? key})
+      : super(key: key);
+  final Offset? mousePointerOffsetGlobal;
+
+  @override
+  Widget build(BuildContext context) {
+    final renderObjectFound = context.findRenderObject() as RenderBox?;
+    double angle = 0.0;
+    if (renderObjectFound != null && mousePointerOffsetGlobal != null) {
+      final localizedMousePointerOffset =
+          renderObjectFound.globalToLocal(mousePointerOffsetGlobal!);
+      final currentWidgetCenterOffset = renderObjectFound.paintBounds.center;
+      final betweenOffset =
+          currentWidgetCenterOffset - localizedMousePointerOffset;
+
+      angle = math.atan2(betweenOffset.dy, betweenOffset.dx) + math.pi;
+    }
+
+    return Transform.rotate(
+      angle: angle,
+      child: Icon(
+        Icons.arrow_right_alt_sharp,
+        size: 42,
+        color: Colors.white,
+      ),
+    );
+    ;
+  }
+}
