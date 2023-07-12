b5b950e53dbf055fbb1062cd3dc706415a346a3a
MJ <minseok.jeong92@gmail.com>
2023-07-12 16:29:03 +0900

    Delete the notosans, notosansJP font, only notosansKR font remain. And change the rendering splash page to using svg for text rendering to reduce the font usage for size reduction.

--- lib/page/splash_page.dart
+++ lib/page/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:hexcolor/hexcolor.dart';
 import '../app_design_resources.dart' show FontName;
 import '../util/screen_size.dart';
@@ -19,17 +20,17 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
 
   bool _visibility = true;
 
-  final _helloTextsInDifferentLanguageTextAndFontMap = <String, String>{
-    'Hello': FontName.NotoSans.name, //미국,영국 Hello(헬로)
-    '你好': FontName.NotoSansJapanese.name, //중국 你好(니하오)
-    'Здравствуйте': FontName.NotoSans.name, //러시아 Здравствуйте(즈드랏스부이쩨)
-    'Bonjuor': FontName.NotoSans.name, //프랑스 Bonjuor(봉쥬흐)
-    'こんにちは': FontName.NotoSansJapanese.name, //일본 こんにちは(곤니찌와)
-    'Guten Tag': FontName.NotoSans.name, //독일 Guten Tag(구텐탁)
-    'Buongiorno': FontName.NotoSans.name, //프랑스 Bonjuor(봉쥬흐)
-    'नमस्ते': FontName.NotoSans.name, //인도 नमस्ते(나마스떼)
-    '안녕하세요': FontName.NotoSansKorean.name,
-  };
+  final _helloSvgFilePaths = <String>[
+    "assets/hello/hello_english.svg",
+    "assets/hello/hello_china.svg",
+    "assets/hello/hello_rusia.svg",
+    "assets/hello/hello_france.svg",
+    "assets/hello/hello_japan.svg",
+    "assets/hello/hello_germany.svg",
+    "assets/hello/hello_italy.svg",
+    "assets/hello/hello_arab.svg",
+    "assets/hello/hello_korea.svg",
+  ];
 
   @override
   void initState() {
@@ -37,15 +38,12 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
     _aniContrl = AnimationController(
         vsync: this, duration: const Duration(milliseconds: 2600));
 
-    final endComputed =
-        (_helloTextsInDifferentLanguageTextAndFontMap.length - 1).toDouble();
+    final endComputed = (_helloSvgFilePaths.length - 1).toDouble();
     _textIndexAnimation =
         Tween(begin: 0.0, end: endComputed).animate(_aniContrl);
 
     _textIndexAnimation = TweenSequence([
-      for (int i = 0;
-          i < _helloTextsInDifferentLanguageTextAndFontMap.length;
-          ++i)
+      for (int i = 0; i < _helloSvgFilePaths.length; ++i)
         TweenSequenceItem(
             tween: Tween(begin: i.toDouble(), end: (i + 1).toDouble()),
             weight: 1),
@@ -88,15 +86,6 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
     });
 
     final windowSize = ScreenSize.getScreenSize(context);
-
-    double fontSizeLarge = 96.0;
-    double fontSizeSmall = 32.0;
-
-    if (ScreenSize.isTabletScreenSize(context) ||
-        ScreenSize.isMobileScreenSize(context)) {
-      fontSizeLarge /= 3.0;
-      fontSizeSmall /= 3.0;
-    }
 
     return Visibility(
       visible: _visibility,
@@ -146,78 +135,20 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
                 width: windowSize.width,
                 height: windowSize.height,
                 color: Colors.black,
+                padding: EdgeInsets.all(32.0),
                 child: Center(
                   child: AnimatedBuilder(
                     animation: _textIndexAnimation,
                     builder: (context, child) {
                       final index = _textIndexAnimation.value.toInt();
-                      final text = _helloTextsInDifferentLanguageTextAndFontMap
-                          .keys
-                          .elementAt(index);
-                      final font =
-                          _helloTextsInDifferentLanguageTextAndFontMap[text];
-                      final baseTextStyle = TextStyle(
-                          fontSize: fontSizeLarge,
-                          color: Colors.white,
-                          fontFamily: font);
-                      final color1 = HexColor('#DADAAA');
-                      final color2 = HexColor('#5C9EFC');
-                      final color3 = HexColor('#B98C76');
-                      final color4 = HexColor('#D3D3D3');
+                      final currentHelloTextSvgFilePath =
+                          _helloSvgFilePaths.elementAt(index);
 
-                      final textToDisplay = Text.rich(
-                        TextSpan(
-                          text: 'print',
-                          style: baseTextStyle.copyWith(
-                            fontSize: fontSizeSmall,
-                            color: color1,
-                          ),
-                          children: [
-                            TextSpan(
-                              text: '(',
-                              style: baseTextStyle.copyWith(
-                                fontSize: fontSizeSmall,
-                                color: color2,
-                              ),
-                            ),
-                            TextSpan(
-                              text: "'",
-                              style: baseTextStyle.copyWith(
-                                fontSize: fontSizeSmall,
-                                color: color3,
-                              ),
-                            ),
-                            TextSpan(
-                              text: text,
-                              style: baseTextStyle.copyWith(
-                                color: color3,
-                              ),
-                            ),
-                            TextSpan(
-                              text: "'",
-                              style: baseTextStyle.copyWith(
-                                fontSize: fontSizeSmall,
-                                color: color3,
-                              ),
-                            ),
-                            TextSpan(
-                              text: ')',
-                              style: baseTextStyle.copyWith(
-                                fontSize: fontSizeSmall,
-                                color: color2,
-                              ),
-                            ),
-                            TextSpan(
-                              text: ';',
-                              style: baseTextStyle.copyWith(
-                                fontSize: fontSizeSmall,
-                                color: color4,
-                              ),
-                            ),
-                          ],
-                        ),
+                      return SvgPicture.asset(
+                        currentHelloTextSvgFilePath,
+                        height: windowSize.width * 0.1,
+                        fit: BoxFit.fitHeight,
                       );
-                      return textToDisplay;
                     },
                   ),
                 ),
